3917333cdac906a2c011b339b0bc7ba2ded1c360
LAPTOP-TSPN8NKB\gr199 <gr1993@naver.com>
2022-07-27 11:13:57 +0900

    로그인 뷰 초안 작성

---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>Longest LogIn</h2>
+    <form @submit="onSubmit">
+      <input placeholder="Enter your email" v-model="email">
+      <input placeholder="Enter your password" v-model="password">
+      <button type="submit">Login</button>
+    </form>
+    <button @click="onSubmit">Test</button>
+  </div>
+</template>
+
+
+<script> 
+export default {
+  name: 'LoginView',
+  components: {
+  },
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      if(!this.email || !this.password) {
+        alert('이메일 또는 패스워드를 입력하세요');
+      }
+
+      try {
+        this.$axios.post(this.Domain + '/login', {
+          email: this.email,
+          password: this.password,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((ex) => {
+          console.log('로그인 실패', ex);
+        })
+      } catch (error) {
+        console.error(error);
+      }
+    }
+	}
+}
+</script>
