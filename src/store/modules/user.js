e030e1d15eb49c360458bb6eb25355c05fd1637e
park kang lim <gr1993@naver.com>
2022-07-28 17:10:46 +0900

    로그인 액션 result json 구조 변경

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -29,10 +29,15 @@ const userStore = {
 		}
 	},
 	actions: {
-		LOGIN({ commit }, { email, password }) {
-			return axios
-				.post(`${hostname}/login`, { email, password })
-				.then(({ data }) => commit("LOGIN", data))
+		LOGIN: async function ({ commit }, { email, password }) {
+			const { data } = await axios.post(`${hostname}/login`, { email, password })
+			
+			if(data.isSuccess) {
+				commit("LOGIN", data.data);
+			} else {
+				throw Error(data.message);
+			}
+			
 		},
 		LOGOUT({ commit }) {
 			commit("LOGOUT")
