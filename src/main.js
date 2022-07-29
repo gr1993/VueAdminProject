31b14d5548aa3a9b702c78bb052d2a10c41d92ad
park kang lim <gr1993@naver.com>
2022-07-29 14:39:38 +0900

    메인 css 셋팅

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import store from './store'
-import axios from 'axios'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import router from './router';
+import store from './store';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+import './assets/css/main.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+Vue.config.productionTip = false;
 
 Vue.prototype.$axios = axios;
 localStorage.hostname = 'https://admin.localhost/admin';
@@ -15,5 +17,5 @@ new Vue({
   vuetify,
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
