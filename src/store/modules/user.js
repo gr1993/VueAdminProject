c96faa8d32706fe76bd529863eaa0ae476d3fddd
park kang lim <gr1993@naver.com>
2022-08-01 11:25:22 +0900

    로그인시 헤더에 토큰 담아 요청할 수 있도록 변경

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,48 +1,54 @@
-import axios from "axios"
+import axios from 'axios';
 
 const { hostname } = localStorage;
 
 const enhanceAccessToken = () => {
-	const { accessToken } = localStorage
-	if (!accessToken) return
-	axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`
+  const { accessToken } = localStorage;
+  if (!accessToken) return;
+  axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 };
-enhanceAccessToken();
 
 const userStore = {
-	namespaced: true,
-	state: {
+  namespaced: true,
+  state: {
     email: null,
-    accessToken: null
-	},
-	getters: {
-	},
-	mutations: {
-		LOGIN: function (state, { accessToken }) {
-			state.accessToken = accessToken;
+    accessToken: null,
+  },
+  getters: {},
+  mutations: {
+    LOGIN: function (state, { accessToken, email }) {
+      state.email = email;
+      state.accessToken = accessToken;
 
-			// 토큰을 로컬 스토리지에 저장
-			localStorage.accessToken = accessToken
-		},
-		LOGOUT(state) {
-			state.accessToken = null
-		}
-	},
-	actions: {
-		LOGIN: async function ({ commit }, { email, password }) {
-			const { data } = await axios.post(`${hostname}/users/login`, { email, password })
-			
-			if(data.isSuccess) {
-				commit("LOGIN", data.data);
-			} else {
-				throw Error(data.message);
-			}
-			
-		},
-		LOGOUT({ commit }) {
-			commit("LOGOUT")
-		}
-	}
+      // 토큰을 로컬 스토리지에 저장
+      localStorage.accessToken = accessToken;
+    },
+    LOGOUT(state) {
+      state.accessToken = null;
+    },
+  },
+  actions: {
+    LOGIN: async function ({ commit }, { email, password }) {
+      const { data } = await axios.post(`${hostname}/users/login`, {
+        email,
+        password,
+      });
+
+      if (data.isSuccess) {
+        commit('LOGIN', {
+          ...data.data,
+          email,
+        });
+
+        enhanceAccessToken();
+      } else {
+        throw Error(data.message);
+      }
+    },
+    LOGOUT({ commit }) {
+      commit('LOGOUT');
+    },
+  },
 };
 
 export default userStore;
