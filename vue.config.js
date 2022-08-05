1c9e3e3dd8e1eac4e80ed70688b4e3b1aa0f09a9
park kang lim <gr1993@naver.com>
2022-08-05 18:15:53 +0900

    웹펙 build시 css쪽 오류나는거 플러그인 설치

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()],
+  },
 });
